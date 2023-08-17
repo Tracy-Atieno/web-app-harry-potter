01f07c005cd27339c3aac5a9e6160461a47769da
TRACY ATIENO <tracyatieno1@gmail.com>
2023-08-17 19:42:42 +0300

    web-app-harry-potter

--- src/app/utils/api.ts
+++
@@ -1,7 +0,0 @@
-// utils/api.ts
-export async function fetchCharacters() {
-    const response = await fetch('https://hp-api.onrender.com/api/characters');
-    const data = await response.json();
-    return data;
-  }
-  
